f92647edf7429056dafee22839b23a82e0042e60
Nakul Rana <102985207+RanaNakul@users.noreply.github.com>
2025-03-25 14:56:33 +0530

    initial commit

---
+++ app.js
@@ -0,0 +1,41 @@
+const express = require("express");
+const app = express();
+
+
+const http = require("http");
+const path = require("path");
+
+const socketio = require("socket.io");
+
+const server = http.createServer(app);
+
+const io = socketio(server);
+
+require("dotenv").config();
+
+const PORT = process.env.PORT || 3000;
+
+
+
+app.set("view engine", "ejs");
+
+app.use(express.static(path.join(__dirname, "public")));
+
+io.on("connection", (socket) => {
+  socket.on("send-location", (data) => {
+    
+    io.emit("receive-location", {id: socket.id, ...data});
+  });
+  socket.on("disconnect", () => {
+    io.emit("user-disconnected",  socket.id);
+  })
+  console.log("New WebSocket connected");
+});
+
+app.get("/", (req, res) => {
+  res.render("index");
+});
+
+server.listen(PORT, () => {
+  console.log(`Server is running on port ${PORT}`);
+});
